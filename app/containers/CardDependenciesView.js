6dee14a9c7a406a59ace9bb9e3c8f49e39a82fa6
Philip Chen <lsps9150414@gmail.com>
2016-07-11 17:48:29 +0800

    Add joint customShape with foriegnObject

--- app/containers/CardDependenciesView.js
+++ app/containers/CardDependenciesView.js
@@ -28,29 +28,69 @@ export default class CardDependenciesView extends React.Component {
     });
     console.log('paper =', paper);
 
-    const rect = new joint.shapes.basic.Rect({
+    const CustomShape = joint.shapes.basic.Generic.extend({
+      markup:
+      '<g class="scalable">' +
+        '<rect/>' +
+        // '<switch>' +
+          '<foreignObject>' +
+            '<div xmlns="http://www.w3.org/1999/xhtml">test</div>' +
+          '</foreignObject>' +
+        // '</switch>' +
+      '</g>',
+
+      defaults: joint.util.deepSupplement({
+        type: 'basic.Rect',
+        attrs: {
+          rect: { fill: 'white', stroke: 'black', 'follow-scale': true, width: 80, height: 40 },
+          foreignObject: { width: 80, height: 40 }
+        }
+      }, joint.shapes.basic.Generic.prototype.defaults)
+    });
+
+
+    const rect = new CustomShape({
       position: { x: 0, y: 0 },
       size: { width: 1, height: 1 },
       attrs: {
-        rect: { fill: 'gray', 'stroke-width': 1, width: 100, height: 100 },
-        text: { text: 'my box', fill: 'white' }
+        rect: { width: 100, height: 100, 'stroke-width': 1 },
+        foreignObject: { width: 100, height: 100 },
+        // text: { text: 'my box', fill: 'white' }
       }
     });
+    console.log(rect);
+    // const rect = new joint.shapes.basic.Rect({
+    //   position: { x: 0, y: 0 },
+    //   size: { width: 1, height: 1 },
+    //   attrs: {
+    //     rect: { fill: 'gray', 'stroke-width': 1, width: 100, height: 100 },
+    //     // text: { text: 'my box', fill: 'white' }
+    //   }
+    // });
 
     const rect2 = rect.clone();
     rect2.translate(300, 300);
+    //
+    // const link = new joint.dia.Link({
+    //   source: { id: rect.id },
+    //   target: { id: rect2.id }
+    // });
 
-    const link = new joint.dia.Link({
-      source: { id: rect.id },
-      target: { id: rect2.id }
-    });
-
-    graph.addCells([rect, rect2, link]);
+    graph.addCells([rect]);
+    graph.addCells([rect2]);
+    // graph.addCells([rect2, link]);
   }
   render() {
     return (
       <div id={'cardDependenciesView'} className={styles.cardDependenciesView} />
     );
+    // <div>
+    //   <svg>
+    //     <foreignObject>
+    //       <div>test</div>
+    //     </foreignObject>
+    //   </svg>
+    // </div>
   }
 }
 
